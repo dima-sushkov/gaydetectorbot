e3be587c1eab118c59a65164f6c6878d1dd435d9
Dmytro Sushkov <dima.sushkov@icloud.com>
2026-04-22 18:13:10 +0300

    Add files via upload

--- src/ChatFunctions.js
+++ src/ChatFunctions.js
@@ -1,14 +1,15 @@
 module.exports = {
-    temporaryMessage(channel, text, lifespan = 10000) {
-        channel.send(text).then((m) => {
-            setTimeout(() => m.delete().catch(() => {}), lifespan);
-        });
+    // Отправка с анимацией печати и задержкой
+    async typingAndSend(channel, text, delay = null) {
+        const pause = delay !== null ? delay : 800 + Math.random() * 1200;
+        await new Promise(r => setTimeout(r, pause));
+        await channel.sendTyping();
+        await new Promise(r => setTimeout(r, 600 + Math.random() * 800));
+        return channel.send(text);
     },
 
     getNickname(msg) {
-        if (!msg.member) {
-            return msg.author.username;
-        }
+        if (!msg.member) return msg.author.username;
         return msg.member.displayName || msg.author.username;
     },
 
